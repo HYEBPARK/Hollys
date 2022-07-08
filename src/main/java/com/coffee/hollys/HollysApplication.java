d92aaf2392db4fe91324a79a70647c17d905c7cc
HYEBPARK <hyeqls3742@gmail.com>
2022-07-08 16:08:42 +0900

    init : 프로젝트 생성

---
+++ src/main/java/com/coffee/hollys/HollysApplication.java
@@ -0,0 +1,13 @@
+package com.coffee.hollys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HollysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HollysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coffee/hollys/HollysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffee.hollys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HollysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
